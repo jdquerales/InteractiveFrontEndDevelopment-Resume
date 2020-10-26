7db85b0037f16f6e1f554ed0e4f8032dd439ad71
jdquerales <jquerales72@gmail.com>
2020-10-26 15:51:03 +0000

    Implement emailjs integration on contact page

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,16 @@
+function sendMail(contactForm) {
+    emailjs.send("gmail.com", "template_p9s2s5k", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  // To block from loading a new page
+}
